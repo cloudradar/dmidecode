ab9ea314523b09ff563038c963162434f7204239
Artur Troian <troian.ap@gmail.com>
2018-12-26 19:01:21 +0200

    doc: add example for Raw
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ examples/raw/dmi.go
@@ -0,0 +1,37 @@
+// +build !windows
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/troian/dmidecode"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("dmidecode")
+
+	buf := bytes.Buffer{}
+	cmd.Stdout = bufio.NewWriter(&buf)
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	dmi, err := dmidecode.Unmarshal(bufio.NewReader(&buf))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	raw, _ := dmi.Raw()
+
+	data, err := json.MarshalIndent(raw, "", "    ")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(string(data))
+	}
+}
